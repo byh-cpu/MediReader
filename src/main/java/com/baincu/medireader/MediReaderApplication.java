572215585a28966db06c67987e9b423e963efbf1
baiyh55203 <baiyh55203@hundsun.com>
2026-03-11 10:52:37 +0800

    Initial commit: 项目初始化

---
+++ src/main/java/com/baincu/medireader/MediReaderApplication.java
@@ -0,0 +1,13 @@
+package com.baincu.medireader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediReaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediReaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baincu/medireader/MediReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baincu.medireader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediReaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
